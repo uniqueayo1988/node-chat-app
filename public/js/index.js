ae9aebb8e5e8d1054af76c0ffc0e099df23ad6d5
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-10-23 15:32:51 +0100

    Broadcast message

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,20 @@ var socket = io();
 socket.on('connect', function() {
   console.log('Connected to Server')
 
-  socket.emit('createEmail', {
-    to: 'jen@example.com',
-    text: 'Hey, this is Andrew.'
-  })
+  // socket.emit('createMessage', {
+  //   to: 'jen@example.com',
+  //   text: 'Hey, this is Andrew.'
+  // })
+
 })
 
 socket.on('disconnect', function() {
   console.log('Disconnected from Server')
 })
 
-socket.on('newEmail', function(email) {
-  console.log('New Email', email)
+socket.on('newMessage', function(message) {
+  console.log('New Message', message)
 })
+  // socket.on('adminMsg', function(message) {
+  //   console.log('Admin Message', message)
+  // })
