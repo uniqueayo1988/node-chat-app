32a3a1cdae0e714cb3c4f05f2af1f60cad6bb885
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-10-24 22:10:46 +0100

    Add message form

--- public/js/index.js
+++ public/js/index.js
@@ -16,11 +16,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('New Message', message)
+  var li = jQuery('<li></li>')
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 })
 
-socket.emit('createMessage', {
-  from: 'John',
-  text: 'Hi'
-}, function (serverMsg) {
-  console.log('Got it', serverMsg)
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault()
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  })
 })
