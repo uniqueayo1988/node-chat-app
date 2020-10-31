721cf4fbaf1e3eb167befabc214c29dad8883690
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-10-31 22:34:25 +0100

    Add geolocation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-var { generateMessage } = require('./utils/message');
+var { generateMessage, generateLocationMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -33,6 +33,10 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('adminMsg', {
       msg: 'Welcome to chat app'
     })
+  })
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
   })
 
   socket.on('disconnect', () => {
