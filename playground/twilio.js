65968fd9e06007e9e338ccc36d3bb60c0cf7c1b1
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-12-05 23:27:07 +0100

    Create chat room

--- playground/twilio.js
+++ playground/twilio.js
@@ -1,7 +1,5 @@
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const accountSid = "ACd54ef47c002cad3b32a1c1292d6f082b";
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-const authToken = "391e86013a0cdf8c41dbc01df7b8f749";
+// const accountSid = "ACd54ef47c002cad3b32a1c1292d6f082b";
+// const authToken = "9aede14b4a1232b956f5112e0a73c271";
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.list({limit: 1})
